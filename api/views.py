14f92920467288e6c33ebd69a39b4f839d712226
ezecon <nationhexus@gmail.com>
2024-10-04 18:55:23 +0600

    added get delete update

--- api/views.py
+++ api/views.py
@@ -1,20 +1,58 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import generics, status
 from .models import BlogPost
 from .serializers import BlogPostSerializer
-# Create your views here.
 
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
-    serializer_class= BlogPostSerializer
+    serializer_class = BlogPostSerializer
 
-    def delete (self, request, *arg, **kwargs):
-        BlogPost.objects.all().delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            "status": status.HTTP_200_OK,
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({
+            "status": status.HTTP_201_CREATED,
+            "data": serializer.data
+        }, status=status.HTTP_201_CREATED)
+    
+    
 class BlogPostListView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
-    serializer_class= BlogPostSerializer
+    serializer_class = BlogPostSerializer
     lookup_field = "pk"
 
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            "status": status.HTTP_200_OK,
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({
+            "status": status.HTTP_200_OK,
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({
+            "status": status.HTTP_204_NO_CONTENT,
+            "message": "Deleted successfully"
+        }, status=status.HTTP_204_NO_CONTENT)
